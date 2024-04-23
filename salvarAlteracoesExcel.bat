8582150e0127816f195afccec46b9252fc402d30
Pedro Batista <phmbbatista@gmail.com>
2024-04-23 12:17:02 -0300

    finalizando

--- salvarAlteracoesExcel.bat
+++ salvarAlteracoesExcel.bat
@@ -1,13 +1,13 @@
 @echo off
 
 rem Defina o caminho da pasta onde você deseja abrir o terminal
-set "caminho_da_pasta=D:\Projeto Sabespe\Sabespe\projeto-sabespe\Sabespe\resources\app"
+set "caminho_da_pasta=D:\Projeto Sabespe\Sabespe\projeto-sabespe\Sabesp\resources\app"
 
 rem Abra o terminal na pasta especificada e execute o comando ng build
 start cmd /k "cd /d %caminho_da_pasta% && npm run build"
 
 rem Aguarde um tempo para o comando ser concluído (ajuste conforme necessário)
-timeout /t 5 >nul
+timeout /t 30 >nul
 
 rem Feche o terminal
 taskkill /f /im cmd.exe
